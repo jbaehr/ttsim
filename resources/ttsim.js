acd7e200034275368028b7a4bff91f4700c1585a
Jonas Bähr <jonas.baehr@web.de>
2020-08-25 13:02:57 +0200

    Detect dlicked OIDs
    With a clicked-event handler on the SVG we find the clicked SVG element
    and extract the OID. Currently there are two options to do this: Using
    dedicated attributes on the pattern declaration, or string-magic on the
    xml-id of the pattern. I'm still undecided which road to follow
    eventually as both approaches have pros and cons.
    The actual hit-test on the SVG is also not perfect: Only the rectangular
    bounding is analysed. But the relevant OID areas usually have more
    complex shapes. Here, too, some additional work is required.

---
+++ resources/ttsim.js
@@ -0,0 +1,73 @@
+/**
+ * This function has to be attached to a mouse event of the SVG's root element.
+ */
+function svg_click(evt) {
+  var intersections = getElementsUnderCurser(evt);
+  var xmlIdsOfFillPatterns = extractXmlIdsOfFillPatterns(intersections);
+
+  var svg_root = evt.currentTarget;
+  function getOidFromPatternElement(xmlIdOfPattern) {
+    // Variant A: we have the OID in the SVG, in a didicated attribtue in "our" name space.
+    var patternElement = svg_root.getElementById(xmlIdOfPattern);
+    // WTF, HTML5 does not support name spaces! cf. https://developer.mozilla.org/en-US/docs/Web/API/Element/getAttributeNS
+    //var tttNs = "http://tttool.entropia.de";
+    //var oid = patternElement.getAttributeNS(tttNs, "oid");
+    var oid = patternElement.getAttribute("ttt:oid");
+    return oid;
+  }
+
+  function getOidFromXmlId(xmlId) {
+    // Variant B: we extract the OID from the XML-ID, that has to follow a "known" pattern
+    var start = xmlId.lastIndexOf("-")
+    var oid = xmlId.slice(start + 1);
+    return oid;
+  }
+
+  //var oids = xmlIdsOfFillPatterns.map(getOidFromPatternElement);
+  var oids = xmlIdsOfFillPatterns.map(getOidFromXmlId);
+
+  handle_clicked_oids(oids);
+}
+
+function getElementsUnderCurser(mouseEvent) {
+  // I found the outline at http://dahlström.net/svg/interactivity/intersection/sandbox_hover.svg thanks a lot!
+  // The current implementation has one severe flaw, though:
+  // It finds the elements which *bounding box* (a rectangle) is under the curser. 
+  // For "complex" shapes this also finds elements where the actual filled area is *not* hit.
+  var svgRoot = mouseEvent.currentTarget;
+  var clickedRect = svgRoot.createSVGRect();
+  clickedRect.x = mouseEvent.clientX;
+  clickedRect.y = mouseEvent.clientY;
+  clickedRect.width = clickedRect.height = 1;
+  var intersections = svgRoot.getIntersectionList(clickedRect, null);
+  return intersections;
+}
+
+function extractXmlIdsOfFillPatterns(svgElements) {
+  var xmlIdsOfFillPatterns = []
+  for (let e of svgElements) {
+    if (e.style.fill) {
+      // parse the id from 'url("#some-id")'
+      var regex = /url\(['"]?#([a-z_][a-z0-9._-]*)['"]?\)/i
+      var match = regex.exec(e.style.fill);
+      if (match) {
+        var id = match[1];
+        xmlIdsOfFillPatterns.push(id);
+      }
+    }
+  }
+
+  return xmlIdsOfFillPatterns;
+}
+
+function handle_clicked_oids(oids) {
+  if (oids.length == 0) {
+    alert("No OID clicked.");
+  }
+  else if (oids.length > 1) {
+    alert("More then one OIDs clicked: " + oids);
+  }
+  else {
+    alert("Clicked OID: " + oids[0]);
+  }
+}
